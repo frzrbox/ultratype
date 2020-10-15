9ec5e8dbf3ccd622e907ebc6d28a9f85cab8397f
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-10-14 23:58:03 -1000

    feat: adds npm message on hero

--- site/index.md
+++ site/index.md
@@ -4,10 +4,5 @@ title: Home
 metaDesc: Dynamic typography for the web
 hero:
     heading: A mini library for splitting type
-    call_to_actions:
-        - to: '/docs'
-          content: View Docs
-        - to: 'https://github.com/frzrbox/ultratype'
-          content: Github
-          target_blank: true
+    subheading: npm i ultratype
 ---
--- site/js/script.js
+++ site/js/script.js
@@ -2,6 +2,7 @@ import {splitByLetter, splitByWord} from './ultratype'
 
 const splitLetterElements = document.querySelectorAll('.split-by-letter');
 const splitWordElements = document.querySelectorAll('.split-by-word');
+const clipboardElements = document.querySelectorAll('.clipboard')
 
 splitLetterElements.forEach(el => {
     splitByLetter(el) 
