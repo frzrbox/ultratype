bf50eab7d710736f86557eeb3395a70b336967c2
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-11-30 15:59:15 -1000

    feat: adds browser

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,31 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import babel from "@rollup/plugin-babel";
-import { terser } from "rollup-plugin-terser";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
-const extensions = [".js", ".jsx", ".ts", ".tsx"];
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 export default {
-  input: "src/index.js",
-  output: [
-    {
-      file: "lib/index.js",
-      format: "umd",
-      name: "Ultratype",
-    }
-  ],
-  plugins: [
-    babel({
-      exclude: "node_modules/**",
-      extensions,
-    }),
-    resolve(),
-    commonjs(),
-    terser(),
-  ],
+	input: 'src/index.js',
+	output: [
+		{
+			file: 'lib/index.js',
+			format: 'umd',
+			name: 'Ultratype',
+		},
+		{
+			file: 'browser/index.js',
+			format: 'iife',
+			name: 'Ultratype',
+		},
+	],
+	plugins: [
+		babel({
+			exclude: 'node_modules/**',
+			extensions,
+		}),
+		resolve(),
+		commonjs(),
+		terser(),
+	],
 };
